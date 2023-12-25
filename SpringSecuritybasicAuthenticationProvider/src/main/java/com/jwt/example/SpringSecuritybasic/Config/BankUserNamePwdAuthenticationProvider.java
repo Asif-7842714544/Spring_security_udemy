2f4cd66f7df0d6abfa47f12b1c339628c23c2202
Your Name <you@example.com>
2023-12-25 21:24:29 +0530

    Adding AuthenticationProvider

--- SpringSecuritybasicAuthenticationProvider/src/main/java/com/jwt/example/SpringSecuritybasic/Config/BankUserNamePwdAuthenticationProvider.java
+++ SpringSecuritybasicAuthenticationProvider/src/main/java/com/jwt/example/SpringSecuritybasic/Config/BankUserNamePwdAuthenticationProvider.java
@@ -1,15 +1,48 @@
 package com.jwt.example.SpringSecuritybasic.Config;
 
+import com.jwt.example.SpringSecuritybasic.Entity.Customer;
+import com.jwt.example.SpringSecuritybasic.Repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class BankUserNamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return null;
+        String email=authentication.getName();
+        String password=authentication.getCredentials().toString();
+        List<Customer> customer=customerRepository.findByEmail(email);
+       if(customer.size()>0){
+           if(passwordEncoder.matches(password,customer.get(0).getPassword())){
+               List<GrantedAuthority> authorities=new ArrayList<>();
+               authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
+               return new UsernamePasswordAuthenticationToken(email,password,authorities);
+           }
+           else{
+               throw new BadCredentialsException("Invalid credentials");
+           }
+
+       }else{
+throw new BadCredentialsException("No user register with this credentials ");
+       }
     }
 
     @Override
